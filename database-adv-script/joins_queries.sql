c306be209b66ae538efdce76cb0ac6b4cfa497b6
ndandax <sebandandala99@gmail.com>
2025-07-01 15:41:56 -0400

    alx

--- database-adv-script/joins_queries.sql
+++ database-adv-script/joins_queries.sql
@@ -5,8 +5,8 @@ INNER JOIN bookings ON users.users_id = bookings.users_id;
 
 SELECT properties.name , reviews.date
 FROM properties
-LEFT JOIN reviews ON properties.users_id = reviews.users_id;
+LEFT JOIN reviews ON properties.users_id = reviews.users_id ORDER BY users.users_id;
 
 SELECT users.name , bookings.date
 FROM users
-OUTER JOIN bookings ON users.users_id = bookings.users_id;
+FULL OUTER JOIN bookings ON users.users_id = bookings.users_id;
